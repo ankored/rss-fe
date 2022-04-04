e6431d881b21799ddfbf895b22f79fb964953696
James Holdren <jamesdholdren@gmail.com>
2022-04-03 23:01:27 -0400

    Got the router going

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	  <header className="App-header">
+			<h1>RSS Reader</h1>
+				<nav
+					style={{
+						color: "#ffffff",
+						paddingBottom: "1rem",
+					}}
+				>
+					<Link to="/login">Log In</Link> |{" "}
+					<Link to="/sign-up">Sign Up</Link>
+				</nav>
+		</header>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import {
+	BrowserRouter,
+	Routes,
+	Route,
+} from "react-router-dom";
+
+import App from './App';
+import Login from './login/page';
+import NotFound from './not-found/page';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="login" element={<Login />} />
+				<Route path="sign-up" element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
